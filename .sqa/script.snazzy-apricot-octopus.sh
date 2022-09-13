506472b580fbf62b462a11a3d7d5d5ec3de0bf3d
orviz <orviz@ifca.unican.es>
2022-09-13 15:31:41 +0200

    Add JePL file structure

--- .sqa/script.snazzy-apricot-octopus.sh
+++ .sqa/script.snazzy-apricot-octopus.sh
@@ -1,4 +1,4 @@
 (
-cd github.com/eosc-synergy/sqaaas-api-spec &&
+cd github.com/EOSC-synergy/sqaaas-api-spec &&
     fuji.py -OID http://hdl.handle.net/10261/157765 --tool_endpoint http://localhost:1071/fuji/api/v1/evaluate -T oai_pmh -E http://digital.csic.es/dspace-oai/request
 )
